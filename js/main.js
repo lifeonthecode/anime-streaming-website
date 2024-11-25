bfeef1d4c2724b6a9ecb84c9c89df7c1ae916593
lifeonthecode <mohammodjuwelislam@gmail.com>
2024-11-25 23:15:04 +0600

    anime streaming website proble solved

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const current_page_active = document.querySelectorAll('.current_page_active');
-console.log(current_page_active)
 const activePage = (links) => {
     links.forEach(link => {
         link.addEventListener('click', (e) => {
@@ -20,12 +19,15 @@ activePage(current_page_active[0].childNodes)
 // show mobile menu bar 
 const show_menu_bar = document.querySelector('.show_menu_bar');
 const close_menu_bar = document.querySelector('.close_menu_bar');
+const mobile_menu_bar_close = document.querySelector('.mobile_menu_bar_close');
 const nav_bar = document.querySelector('.nav_bar');
 
 
 show_menu_bar.addEventListener('click', () => {
     nav_bar.classList.add('show')
+    mobile_menu_bar_close.classList.add('show')
 })
 close_menu_bar.addEventListener('click', () => {
     nav_bar.classList.remove('show')
+    mobile_menu_bar_close.classList.remove('show')
 })
